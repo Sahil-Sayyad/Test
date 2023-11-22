610baa84d4f2a3584ccab33c4668b1ab925c640f
Backend Engineer <96423459+Sahil-Sayyad@users.noreply.github.com>
2023-11-22 07:16:17 +0000

    Operating System  Lab  Ass.1 2 Questions Done

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void display() {
+        int arr[] = { 1, 2, 2, 4, 3, 4 };
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+    }
+
+    public static void main(String args[]) {
+        System.out.println("Sahil Sayyad");
+        display();
+    }
+}
--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-app.js
+const express = require("express");
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send('Sahil Sayyad Here');
+});
+
+app.listen(8000);
+console.log('Server is running on port 8000');
