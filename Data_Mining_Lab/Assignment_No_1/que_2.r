d16c66f401e5122bb93ef5b0732d4b33905ac1d6
Backend Engineer <96423459+Sahil-Sayyad@users.noreply.github.com>
2023-11-26 11:53:30 +0000

    2nd question added

---
+++ Data_Mining_Lab/Assignment_No_1/que_2.r
@@ -0,0 +1,10 @@
+# Write a R program to sort a list of strings in 
+# ascending & descending Order.
+stud_name = c("Rushab", "Rohit", "Sahil")
+print(stud_name)
+cat("Names in ascending Order ", sort(stud_name) , "\n")
+cat("Names in dscending Order ", sort(stud_name,decreasing = TRUE))
+
+# [1] "Rushab" "Rohit"  "Sahil" 
+# Names in ascending Order  Rohit Rushab Sahil 
+# Names in dscending Otrder  Sahil Rushab Rohit
